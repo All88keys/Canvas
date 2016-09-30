2558c50d22988218c212e131ae60178bbb610382
All88keys <chrisvass1@gmail.com>
2016-09-30 17:52:32 -0400

    f

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
+
+var power = document.getElementById('power');
 var x = 2;
+var y = 2;
+power.innerHTML = x;
 function square(){
-  var power = document.getElementById('power');
-  power.innerHTML = Math.pow(x,2);
+  math.config({precision: 2000});
+  var z = math.bignumber(Math.pow(x,y));
+  y++;
+  power.innerHTML = math.format(z, {notation: 'fixed'});
 }
